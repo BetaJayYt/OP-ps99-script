8d536e7fed1b6e450e42a505defce945cc15e5b4
BetaJayYt <javienscodm@gmail.com>
2025-01-07 00:40:44 -0600

    Update Op-hub.lua

--- Op-hub.lua
+++ Op-hub.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://egorikusa.space/fbae5ea73f97338ae79cd468.lua", true))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Fedzen/script/refs/heads/main/.lua", true))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mrxver0/Scripts/refs/heads/main/PS99.lua"))()
